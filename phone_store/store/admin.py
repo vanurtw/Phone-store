fc69d8f5da469f1bcbb0f17cb1217f73c69855d7
Ivan <vanuartw@mail.ru>
2023-12-15 15:00:35 +0300

    model phone

--- phone_store/store/admin.py
+++ phone_store/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Manufacture, PhoneProduct
+
 
 # Register your models here.
+
+@admin.register(Manufacture)
+class ManufactureAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PhoneProduct)
+class PhoneProductAdmin(admin.ModelAdmin):
+    pass
--- phone_store/store/models.py
+++ phone_store/store/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
+
 # Create your models here.
+class Manufacture(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AbstractProduct(models.Model):
+    MemoryChoices = [
+        ('M_128', '128GB'),
+        ('M_256', '256 GB'),
+        ('M_512', '512 GB'),
+        ('M_1', '1 TB'),
+        ('M_2', '2 TB'),
+
+    ]
+
+    ColorChoices = [
+        ('BLACK', 'Black'),
+        ('WHITE', 'White'),
+        ('BLUE', 'Blue'),
+        ('BEIGE', 'Beige'),
+    ]
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    sale = models.BooleanField(default=False)
+    original_price = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField()
+    diagonal = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='media/store/}', blank=True, null=True)
+    description = models.TextField()
+    manufacture = models.ForeignKey(Manufacture, on_delete=models.CASCADE, related_name='manufactures')
+    memory = models.CharField(max_length=6, choices=MemoryChoices)
+    color = models.CharField(max_length=6, choices=ColorChoices)
+    frame = models.CharField(max_length=50)  # korpus
+    height = models.DecimalField(max_digits=10, decimal_places=2)
+    width = models.DecimalField(max_digits=10, decimal_places=2)
+    Thickness = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        abstract = True
+
+
+class PhoneProduct(AbstractProduct):
+    ConnectorChoices = [
+        ('USB', 'USB'),
+        ('TPC', 'Type-C'),
+        ('LIGHT', 'Lightning'),
+        ('MC_USB', 'Micro USB'),
+    ]
+
+    BatteryChoices = [
+        ('LI', 'Li-Ion'),
+        ('NI', 'Ni-Cd'),
+        ('NI_MH', 'Ni-MH')
+    ]
+
+    display = models.CharField(max_length=20)
+    Connector = models.CharField(max_length=9, choices=ConnectorChoices)
+    Battery_type = models.CharField(max_length=9, choices=BatteryChoices)
+
+    def __str__(self):
+        return self.name
