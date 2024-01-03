dfab6b9b44ab4589bfef0083273617184f05ce88
Ivan <vanuartw@mail.ru>
2024-01-03 20:37:57 +0300

    coupon model

--- phone_store/cart/admin.py
+++ phone_store/cart/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Coupon
+
 
 # Register your models here.
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['coupon', 'start_date', 'end_date']
+    list_filter = ['active']
--- phone_store/cart/models.py
+++ phone_store/cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Coupon(models.Model):
+    coupon = models.CharField(max_length=20, verbose_name='Купон')
+    active = models.BooleanField(default=True, verbose_name='Активный')
+    start_date = models.DateTimeField(verbose_name='Дата начала')
+    end_date = models.DateTimeField(verbose_name='Дата конца')
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.coupon
+
+    class Meta:
+        verbose_name = 'Купон'
+        verbose_name_plural = 'Купоны'
