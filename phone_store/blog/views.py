9032f5fe44ebccd7a01715bc0e457c14a04887a4
Ivan <vanuartw@mail.ru>
2024-01-29 19:08:21 +0300

    blog search

--- phone_store/blog/views.py
+++ phone_store/blog/views.py
@@ -5,10 +5,16 @@ from .models import Post, Categories
 # Create your views here.
 def blog_home(request):
     context = {'chapter': 'blog'}
-    category = request.GET.get('category')
-    if category:
-        category = Categories.objects.get()
-    post = Post.objects.all()
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        post = Post.objects.filter(title__icontains=search)
+    else:
+        category_slug = request.GET.get('category', None)
+        if category_slug:
+            category = Categories.objects.get(slug=category_slug)
+            post = category.items.all()
+        else:
+            post = Post.objects.all()
     context['post'] = post
 
     return render(request, 'blog/blog.html', context)
