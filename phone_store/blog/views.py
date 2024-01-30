6caebf13a7e0d15710edf42ab980d5175acb42df
Ivan <vanuartw@mail.ru>
2024-01-30 19:33:36 +0300

    blog detail

--- phone_store/blog/views.py
+++ phone_store/blog/views.py
@@ -13,19 +13,22 @@ def blog_home(request):
         post = Post.objects.filter(Q(title__icontains=search) | Q(tags__name__icontains=search))
     else:
         category_slug = request.GET.get('category', None)
-
         if category_slug:
             category = Categories.objects.get(slug=category_slug)
             post = category.items.all()
         else:
-            post = Post.objects.all()
+            tag_slug = request.GET.get('tag', None)
+            if tag_slug:
+                post = Post.objects.filter(tags__slug__in=[tag_slug])
+            else:
+                post = Post.objects.all()
     context['post'] = post
 
     return render(request, 'blog/blog.html', context)
 
 
 def blog_details(request, slug):
-    context = {'chapter':'blog'}
+    context = {'chapter': 'blog'}
     post = get_object_or_404(Post, slug=slug)
     context['post'] = post
     return render(request, 'blog/blog-details.html', context)
