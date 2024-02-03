82583541a35aec6813b89bb0e966b367f75995ee
Ivan <vanuartw@mail.ru>
2024-02-03 14:49:32 +0300

    store

--- phone_store/store/templatetags/tags_load.py
+++ phone_store/store/templatetags/tags_load.py
@@ -25,15 +25,19 @@ def product_header(context):
 def product_header_content(context):
     prod_com = context['product'].comments.all().select_related('user')
     return {'prod_header': context['prod_header'], 'product': context['product'],
-            'comment_count': context['comment_count'],'prod_com':prod_com,
+            'comment_count': context['comment_count'], 'prod_com': prod_com,
             'color_product': context['color_product'], 'form': context['form'], 'user': context['user']}
 
 
 @register.filter(name='range')
 def range_filter(count):
+    if not count:
+        count = 5
     return [i for i in range(int(count))]
 
 
 @register.filter(name='range_none')
 def range_filter_none(count):
+    if not count:
+        count = 5
     return [i for i in range(int(count), 5)]
